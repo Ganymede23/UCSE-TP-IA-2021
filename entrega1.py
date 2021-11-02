6907c791c5485a5c35814fffcf7a203050f17fdb
kicillof <cuentasdebusso@hotmail.com>
2021-11-01 22:34:26 -0300

    Actions simplificado y limpieza final del codigo

--- entrega1.py
+++ entrega1.py
@@ -1,14 +1,11 @@
-
 from simpleai.search.models import SearchProblem
-from simpleai.search import astar, breadth_first, depth_first, greedy, uniform_cost, iterative_limited_depth_first
+from simpleai.search import astar
 from simpleai.search.viewers import WebViewer, BaseViewer, ConsoleViewer
-from math import ceil
 
 ACCIONES_MOVER = [((-1, 0)),
                   ((1, 0)),
                   ((0, -1)),
                   ((0, 1))]
-
 
 class robotsminerosproblem(SearchProblem):
 
@@ -31,69 +28,66 @@ class robotsminerosproblem(SearchProblem):
         # el destino puede venir definido por una posicion (fila, columna) o por un id de robot escaneador
         #("s1", "mover", (5, 1))
         #("s1", "cargar", "e2")
-        
-        #comienzo recorriendo los robots disponibles
+
+        # comienzo recorriendo los robots disponibles
         for robot in robots:
-            #Si el robot es escaneador
+            # Si el robot es escaneador
             if robot[1] == "escaneador":
 
-                #me guardo el id, la posicion y la bateria actual del robot
+                # me guardo el id, la posicion y la bateria actual del robot
                 id_robot, tipo_robot, posicion, bateria = robot
 
-                #lo que hago es sacar la fila y columna en la que se encuentra el robot
+                # lo que hago es sacar la fila y columna en la que se encuentra el robot
                 fila_robot, columna_robot = posicion
 
-                #si la bateria actual es mayor que 100mAh, el robot se puede mover
-                if bateria >= 100:               
+                # si la bateria actual es mayor que 100mAh, el robot se puede mover
+                if bateria >= 100:
                     # recorro las acciones_mover (arriba, abajo, derecha, izquierda), calculo la nueva posición y la agrego en acciones
                     for (fila, columna) in ACCIONES_MOVER:
                         nueva_fila = fila_robot + fila
                         nueva_columna = columna_robot + columna
                         posicion_destino = (nueva_fila, nueva_columna)
-                        #si la nueva pos está en los tuneles, muevo el robot
+                        # si la nueva pos está en los tuneles, muevo el robot
                         if (posicion_destino in TUNELES):
-                            acciones.append((id_robot, "mover", posicion_destino))                
-            else: #si el robot es de soporte
-                #me guardo el id, la posicion y la bateria actual del robot
+                            acciones.append(
+                                (id_robot, "mover", posicion_destino))
+            else:  # si el robot es de soporte
+                # me guardo el id, la posicion y la bateria actual del robot
                 id_robot, tipo_robot, posicion = robot
 
-                #lo que hago es sacar la fila y columna en la que se encuentra el robot
+                # lo que hago es sacar la fila y columna en la que se encuentra el robot
                 (fila_robot, columna_robot) = posicion
 
-                #recorro la lista de robots
+                # recorro la lista de robots
                 for robot_a_abastecer in robots:
-                    #pregunto si la posicion del robot a abastecer es la misma que el robot de soporte
-                    #si la bateria del robot a abastecer es menor a 1000 
+                    # pregunto si la posicion del robot a abastecer es la misma que el robot de soporte
+                    # si la bateria del robot a abastecer es menor a 1000
                     # y si el robot es tipo escaneador
                     cargo = False
                     if robot_a_abastecer[1] == "escaneador":
-                                         
-                        if (robot_a_abastecer[2] == posicion and (robot_a_abastecer [3] < 1000)):
-                            cargo = True
-                            #si cumple estas caracteristicas debo generar una acción de cargar
-                            #en acciones cargo el id del robot de soporte, la accion y el id del robot escaneador que se abasteció
-                            acciones.append((id_robot, "cargar", robot_a_abastecer[0]))
-                            
-                            
-                #si no se cumple con las condiciones anteriores, el robot se debe mover]
-                if not cargo:
-                    for (fila, columna) in ACCIONES_MOVER:
 
-                        nueva_fila = fila_robot + fila
-                        nueva_columna = columna_robot + columna
-                        posicion_destino = (nueva_fila, nueva_columna)
-                        #si la nueva posicion esta en la lista de tuneles me puedo mover
-                        if (posicion_destino in TUNELES): 
-                            acciones.append((id_robot, "mover", posicion_destino))
-        
-        
+                        if (robot_a_abastecer[2] == posicion and (robot_a_abastecer[3] < 1000)):
+                            cargo = True
+                            # si cumple estas caracteristicas debo generar una acción de cargar
+                            # en acciones cargo el id del robot de soporte, la accion y el id del robot escaneador que se abasteció
+                            acciones.append(
+                                (id_robot, "cargar", robot_a_abastecer[0]))
+
+                for (fila, columna) in ACCIONES_MOVER:
+                    nueva_fila = fila_robot + fila
+                    nueva_columna = columna_robot + columna
+                    posicion_destino = (nueva_fila, nueva_columna)
+                    # si la nueva posicion esta en la lista de tuneles me puedo mover
+                    if (posicion_destino in TUNELES):
+                        acciones.append((id_robot, "mover", posicion_destino))
+
         return acciones
 
 # ("s1", "soporte", (5,1), 1000) <--- ESTADO DEL ROBOT
 # (('e1', 'escaneador', (5,0), 1000), ('s1', 'soporte', (5,0)))
 
     def result(self, state, action):
-        
+
         id_robot, accion_desc, accion = action
 
         robots, tuneles = state
@@ -105,7 +99,7 @@ class robotsminerosproblem(SearchProblem):
             # busco el robot que se debe mover
             robot = [x for x in robots if x[0] == id_robot]
             robot = robot[0]
-            indice =robots.index(robot)
+            indice = robots.index(robot)
             robots.remove(robot)
 
             robotlist = list(robot)
@@ -126,7 +120,7 @@ class robotsminerosproblem(SearchProblem):
             # le relleno la bateria y lo vuelvo insertar
             robot = [x for x in robots if x[0] == accion]
             robot = robot[0]
-            indice =robots.index(robot)
+            indice = robots.index(robot)
             robots.remove(robot)
 
             robotlist = list(robot)
@@ -135,17 +129,17 @@ class robotsminerosproblem(SearchProblem):
             robots.insert(indice, tuple(robotlist))
 
         new_state = tuple(robots), tuple(tuneles)
-                
+
         return new_state
 
     def heuristic(self, state):
-        # Cantidad de tuneles restantes + si hay más de 10, 
-        # va a necesitar al menos una recarga, o en su defecto tardaría 
+        # Cantidad de tuneles restantes + si hay más de 10,
+        # va a necesitar al menos una recarga, o en su defecto tardaría
         # más que esos 5 que se suman.
         estimacion = 0
         robots, tuneles = state
 
-        estimacion += ceil(len(tuneles)/2)
+        estimacion += len(tuneles)
 
         return estimacion
 
@@ -164,56 +158,31 @@ def planear_escaneo(tuneles, robots):
         ROBOTS.append(robot)
 
     # Asi quedan armado el estado:
-    # TUNELES=((5,1),(6,1),(6,2))
+    # TUNELES= ((5,1),(6,1),(6,2))
     # ROBOTS = [("s2", "soporte",(5,1)),("s1", "soporte",(5,1)), ("e1", "escaneador", (5,1), 1000), ("e2", "escaneador", (5,1), 1000),("e3", "escaneador", (5,1), 1000)]
 
     INITIAL_STATE = (tuple(ROBOTS), tuple(TUNELES))
 
     METODOS = {
-        'breadth_first': breadth_first,
-        'depth_first': depth_first,
-        'iterative_limited_depth_first': iterative_limited_depth_first,
-        'uniform_cost': uniform_cost,
         'astar': astar,
     }
 
     problem = robotsminerosproblem(INITIAL_STATE)
-    #visor = BaseViewer()
-    #result = METODOS['astar'](problem, graph_search=True, viewer=visor)
     result = METODOS['astar'](problem, graph_search=True)
     plan = []
-    '''
-    print('Camino: ')
-    print('|           ACCION           |  				        				ESTADO  					        			|')
-    
-    
-    for x in result.path():
-        print(' - ', x)
-
-    print()
-    print('Estado final:', result.state)
-    print()
-    print(' - Estadisticas:')
-    print(' - Cantidad de acciones hasta meta:', len(result.path()))
-    print(' - Raw data:', visor.stats)
-    '''
-    # print(' - Raw data:', visor.stats)
-    # print('Resultado: ', result)
     for action in result.path():
         if action[0] is not None:
             plan.append(action[0])
 
     return plan
 
-
 if __name__ == '__main__':
-    #start_time = time.time()
 
     tuneles = (
-    (3, 3),
-    (4, 3),
-    (5, 1), (5, 2), (5, 3),
-    (6, 3),
+        (3, 3),
+        (4, 3),
+        (5, 1), (5, 2), (5, 3),
+        (6, 3),
     )
 
     robots = (("e1", "escaneador"), ("s1", "soporte"))
